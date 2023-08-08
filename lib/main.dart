844fbb2f2aef4992baa032297b8f91caf56be72a
Knot <akthakorn@gmail.com>
2023-08-08 13:56:53 +0700

    add Todo App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mvc/routes/routes_page.dart';
-import 'package:flutter_mvc/views/home.dart';
-import 'package:flutter_mvc/views/pokemon_list_view.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_mvc/screens/home.dart';
+
+// import 'package:flutter_mvc/routes/routes_page.dart';
+// import 'package:flutter_mvc/views/home.dart';
+// import 'package:flutter_mvc/views/pokemon_list_view.dart';
+// import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +16,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter MVC',
+    return App();
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   // theme: ThemeData(
+    //   //     primaryColor: Colors.blue,
+    //   //     brightness: Brightness.light,
+    //   //     visualDensity: VisualDensity.adaptivePlatformDensity),
+    //   home: HomeTodo(),
+    //   // home: Home(),
+    //   // home: PokemonListView(),
+    //   // getPages: RoutePages.pages,
+    //   // routingCallback: (routing) {
+    //   //   if (routing!.current == '/') {
+    //   //     debugPrint("Home Scene");
+    //   //   }
+    //   // },
+    // );
+  }
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.blue,
-          brightness: Brightness.light,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: Home(),
-      // home: PokemonListView(),
-      getPages: RoutePages.pages,
-      routingCallback: (routing) {
-        if (routing!.current == '/') {
-          debugPrint("Home Scene");
-        }
-      },
+      home: HomeTodo(),
     );
   }
 }
